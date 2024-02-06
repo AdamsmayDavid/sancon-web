fc89924659cdf6132eeb979490c32bac9b942aa7
AdamsmayDavid <admdvd14@gmail.com>
2024-02-06 08:43:52 +0800

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,10 @@
-    
-    $(document).ready(function() {
+      $(document).ready(function() {
         setTimeout(function() {
         $('#container').addClass('loaded');
         // Once the container has finished, the scroll appears
         if ($('#container').hasClass('loaded')) {
             // It is so that once the container is gone, the entire preloader section is deleted
-            $('#preloader').delay(600).queue(function() {
+            $('#preloader').delay(0).queue(function() {
             $(this).remove();
             });}
         }, 2000);});
@@ -15,12 +14,13 @@
 
     window.addEventListener('scroll', function () {
         if(window.pageYOffset > 100) {
-            nav.classList.add('bg-primary', 'shadow');
+            nav.classList.add('bg-primary',  'shadow');
 
         } else {
             nav.classList.remove('bg-primary', 'shadow');
         }
     });
+
 
 // var hihi ="10" + + +"10" 
 // + "=" + "jeng jeng"
@@ -33,3 +33,17 @@
     // setInterval('autoRefresh()', 5000);
     window.location.replace("#");
 
+//carousel
+jQuery('form.wpcf7-form input[type="submit"]').each( function( i, val )
+    {
+            jQuery(this).on("click", function(e) {
+
+                var form_id = jQuery(this).closest('.wpcf7').prop('id') ;
+
+                setTimeout(function( )
+                {
+                    $( "#" + form_id + " form.wpcf7-form .nen-wpcf7-submit" ).attr('disabled','disabled');
+
+                } , 10 , form_id );
+            });
+    });
